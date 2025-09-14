9f3df79191269456afc79f982ec16aea797de4b7
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 19:01:08 -0400

    Add Lesson_1_Introduction_To_Typescript/part_2_typeDefinitions.ts

---
+++ Lesson_1_Introduction_To_Typescript/part_2_typeDefinitions.ts
@@ -0,0 +1,5 @@
+let name: string = "Alice";
+let age: number = 30;
+let isStudent: boolean = true;
+let scores: number[] = [90, 80, 70]; // Array of numbers
+let user: [string, number] = ["Alice", 30]; // Tuple with specific types
