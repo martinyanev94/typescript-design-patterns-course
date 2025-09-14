6fd8512354192609a0ad1a257cb6603832e5da88
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 19:03:43 -0400

    Add Lesson_7_Singleton_Pattern_Overview/part_5_singletonLogger.ts

---
+++ Lesson_7_Singleton_Pattern_Overview/part_5_singletonLogger.ts
@@ -0,0 +1,23 @@
+class Logger {
+    private static instance: Logger;
+
+    private constructor() {}
+
+    public static getInstance(): Logger {
+        if (!Logger.instance) {
+            Logger.instance = new Logger();
+        }
+        return Logger.instance;
+    }
+
+    public log(message: string): void {
+        console.log(`[Log]: ${message}`);
+    }
+}
+const logger1 = Logger.getInstance();
+logger1.log("First log message");
+
+const logger2 = Logger.getInstance();
+logger2.log("Second log message");
+
+console.log(logger1 === logger2); // true
