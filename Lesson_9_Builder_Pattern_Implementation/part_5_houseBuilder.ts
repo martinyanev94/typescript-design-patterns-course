f521bce3821160939c799778489379da4482920f
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 19:04:01 -0400

    Add Lesson_9_Builder_Pattern_Implementation/part_5_houseBuilder.ts

---
+++ Lesson_9_Builder_Pattern_Implementation/part_5_houseBuilder.ts
@@ -0,0 +1,18 @@
+const myHouse = new HouseBuilder(3)
+    .addGarden()
+    .addGarage()
+    .build();
+
+console.log(myHouse.describeHouse());
+
+typescript
+const myHouse = new HouseBuilder(3)
+    .addGarden()
+    .addGarage()
+    .build();
+
+console.log(myHouse.describeHouse());
+
+
+House with 3 rooms, Garden: Yes, Garage: Yes
+
