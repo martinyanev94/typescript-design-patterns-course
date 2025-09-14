0de03635df29c33695ce24fd93395282d1ea52de
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 19:01:02 -0400

    Add Lesson_19_Understanding_Key_Concepts_In_Functional_Programming/part_4_doubleArrayValues.ts

---
+++ Lesson_19_Understanding_Key_Concepts_In_Functional_Programming/part_4_doubleArrayValues.ts
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map(n => n * 2); // Original array remains unchanged
+console.log(numbers); // Output: [1, 2, 3, 4, 5]
+console.log(doubled); // Output: [2, 4, 6, 8, 10]
