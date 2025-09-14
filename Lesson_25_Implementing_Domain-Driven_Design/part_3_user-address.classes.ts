ac00842b7cbb0adfd6d9ffb6f7e0ef159a955b2f
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 19:02:00 -0400

    Add Lesson_25_Implementing_Domain-Driven_Design/part_3_user-address.classes.ts

---
+++ Lesson_25_Implementing_Domain-Driven_Design/part_3_user-address.classes.ts
@@ -0,0 +1,7 @@
+class User {
+    constructor(public id: string, public name: string) {}
+}
+
+class Address {
+    constructor(public street: string, public city: string, public postalCode: string) {}
+}
