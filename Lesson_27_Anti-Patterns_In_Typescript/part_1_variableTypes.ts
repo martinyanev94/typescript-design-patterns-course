e1c71567374203c52867d3fccf6a8fb57e7588d2
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 19:02:12 -0400

    Add Lesson_27_Anti-Patterns_In_Typescript/part_1_variableTypes.ts

---
+++ Lesson_27_Anti-Patterns_In_Typescript/part_1_variableTypes.ts
@@ -0,0 +1,11 @@
+let value: any;
+
+value = "Hello World";
+console.log(value); // Outputs: Hello World
+
+value = 42;
+console.log(value); // Outputs: 42
+let value: string | number;
+
+value = "Hello World"; // Valid
+value = 42; // Also valid
